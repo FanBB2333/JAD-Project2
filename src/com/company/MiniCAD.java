8eb5a75ee6d791769abbc0e07edcd294c8564d90
l1ght <2504082957@qq.com>
2021-11-08 16:13:01 +0800

    initialize MiniCAD

---
+++ src/com/company/MiniCAD.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MiniCAD extends JFrame {
+    public static void main(String[] agrs){
+        Frame jf = new W7Draw();
+        jf.setTitle("W7");
+        jf.setSize(800,600);
+        jf.setVisible(true);
+        jf.setLocationRelativeTo(null); // move to center
+
+        Graphics g = jf.getGraphics();
+
+    }
+
+    public MiniCAD(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(Color.white);
+
+    }
+}
+
+
+
+
+class Pair<T>{
+    public T x;
+    public T y;
+    Pair(T _x, T _y){
+        x = _x;
+        y = _y;
+    }
+}
+
+
+class Shape {
+    public Pair<Integer> p1 = new Pair<Integer>(0, 0);
+    public Pair<Integer> p2 = new Pair<Integer>(0, 0);
+
+
+
+}
