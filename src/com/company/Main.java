018a514776561eca1a15f8f04f344e6bc11f574e
l1ght <2504082957@qq.com>
2021-11-10 13:09:03 +0800

    add output

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,18 +20,88 @@ public class Main {
 
 
 class Repo{
-    private ArrayList<Integer> tasks = new ArrayList<>();
-    private int cnt = 0;
+    public ArrayList<Integer> tasks = new ArrayList<>();
     public Repo(String items){
         String[] _tasks = items.split(" ");
         for (String task : _tasks) {
             tasks.add(Integer.parseInt(task));
         }
-        cnt = _tasks.length;
     }
-    public int getSize(){
-        return cnt;
+//    public int getSize(int id){
+//        synchronized (this){
+//            try{
+//                int task = tasks.remove(0);
+//                System.out.printf("Thread-%d finish %d\n", id, task);
+//
+//            }
+//
+//
+//            return tasks.size();
+//        }
+//    }
+
+    public synchronized int p1(){
+        int task = tasks.remove(0);
+        System.out.printf("Thread-%d finish %d\n", 0, task);
+        this.notify();
+        try {
+            this.wait();
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return tasks.size();
+    }
+
+    public synchronized int p2(){
+        int task = tasks.remove(0);
+        System.out.printf("Thread-%d finish %d\n", 1, task);
+        this.notify();
+        try {
+            this.wait();
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        if(task == 0){
+
+        }
+        return tasks.size();
     }
 
 
+}
+class Worker1 implements Runnable{
+
+    public Repo r = null;
+
+    @Override
+    public void run() {
+        while (r.p1() > 0){
+            System.out.println(" 1: " + r.tasks.size());
+        }
+
+    }
+
+    public Worker1(Repo repo){
+        r = repo;
+    }
+
+}
+
+class Worker2 implements Runnable{
+    public Repo r = null;
+
+    @Override
+    public void run() {
+        while (r.p2() > 0){
+            System.out.println(" 2: " + r.tasks.size());
+
+        }
+
+    }
+
+    public Worker2(Repo repo){
+        r = repo;
+    }
 }
